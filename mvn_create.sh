ea83eddbb0840ced9f750d1e197fd9e2333666cb
seralf <seralf@gmail.com>
2016-04-28 19:12:16 +0200

    updating versions of libreries. Updated JVM to JDK 1.8

--- mvn_create.sh
+++ mvn_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo USAGE: mvn_create groupId artifactId package
-mvn -B archetype:generate -DarchetypeGroupId=it.seralf.development -DarchetypeArtifactId=maven-scala-java-archetype -DarchetypeVersion=0.0.1-SNAPSHOT -DgroupId=$1 -DartifactId=$2 -Dpackage=$3
+mvn -B archetype:generate -DarchetypeGroupId=it.seralf.development -DarchetypeArtifactId=maven-scala-java-archetype -DarchetypeVersion=0.0.2 -DgroupId=$1 -DartifactId=$2 -Dpackage=$3
 
 
